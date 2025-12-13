474ad15afd5690c1535746ad8230e8e9a07b361d
TeialD <teial.starks@gmail.com>
2025-12-12 21:14:06 -0800

    Remove unused item

--- web/src/components/AddCollaborationModal.tsx
+++ web/src/components/AddCollaborationModal.tsx
@@ -34,7 +34,6 @@ function AddCollaborationModal({
   isOpen,
   onClose,
   applicationId,
-  essays,
   onSuccess,
 }: AddCollaborationModalProps) {
   const { showSuccess, showError } = useToastHelpers();
